cac3e2146fbba0fcca867b67b3221edfee01d58b
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-03-08 23:39:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        // add your code here
+         ArrayList<Integer> arr = new ArrayList<>(); 
+        
+        Node temp = head; 
+        
+        while(temp != null) 
+        {
+            arr.add(temp.data); 
+            temp = temp.next;
+        } 
+        Collections.sort(arr); 
+        
+        Node dummy = new Node(0); 
+        Node curr = dummy; 
+        
+        for(int i = 0; i < arr.size(); i++)
+        {
+            Node node = new Node(arr.get(i));
+            curr.next = node; 
+            curr = node;
+        } 
+        return dummy.next;
+    }
+}
+
+
