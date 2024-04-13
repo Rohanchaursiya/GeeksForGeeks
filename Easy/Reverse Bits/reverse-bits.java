9418a87d9d514199f7e71dd7ed97f49baa7cb350
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-04-13 23:17:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long X = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.reversedBits(X));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static Long reversedBits(Long x) {
+        // code here
+        long res = 0;
+        int n = 32;
+        for (int i = 0; i < n; i++) {
+            res <<= 1;
+            if ((x & 1) == 1) {
+                res ^= 1;
+            }
+            x >>= 1;
+        }
+        return res;
+    }
+};
