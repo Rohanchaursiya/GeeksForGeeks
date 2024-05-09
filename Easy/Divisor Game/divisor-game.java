d6ca30a5aba361bb945a4ce0a5d512313ee513cf
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-05-09 22:13:43 +0530

    Updated Solution - GfG to GitHub

--- Easy/Divisor Game/divisor-game.java
+++ Easy/Divisor Game/divisor-game.java
@@ -1,36 +1,34 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.*;
+import java.io.*;
 import java.lang.*;
 import java.math.*;
-import java.io.*;
+import java.util.*;
 
 class GFG {
-  public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-    int T = sc.nextInt();
-    while (T-- > 0) {
-      int n = sc.nextInt();
-      Solution obj = new Solution();
-      String ans = obj.divisorGame(n) ? "True" : "False";
-      System.out.println(ans);
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            String ans = obj.divisorGame(n) ? "True" : "False";
+            System.out.println(ans);
+        }
     }
-  }
 }
 
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
 class Solution {
-  public static boolean divisorGame(int N) {
-    // code here
-    if(N<=1)
-    return false;
-    if(N%2==0)return true;
-    return false;
-  }
+    public static boolean divisorGame(int n) {
+        // code here
+        if(n<=1)return false;
+        if(n%2==0)return true;
+        return false;
+    }
 }
-     
