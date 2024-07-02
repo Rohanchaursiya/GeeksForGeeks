f65c0b93367d396469aa27ad8e8ad256884830c2
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-07-02 09:43:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    String data;
+    Node next;
+
+    Node(String x) {
+        data = x;
+        next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    LinkedList() { head = null; }
+
+    void addNode(String str) {
+        if (head == null) {
+            head = new Node(str);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(str);
+        }
+    }
+
+    void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            LinkedList list = new LinkedList();
+
+            for (int i = 0; i < K; i++) {
+                String str = sc.next();
+                list.addNode(str);
+            }
+
+            Solution ob = new Solution();
+            boolean ans = ob.compute(list.head);
+            if (ans) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static boolean isPalin(String s){
+        if(s.length()==1)return true;
+        int i=0;
+        while(i<(s.length()/2)){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i))return false;
+            else{
+                i++;
+            }
+        }
+        return true;
+    }
+    public boolean compute(Node root) {
+        Node temp=root;
+        String s="";
+        while(temp!=null){
+            s=s+temp.data;
+            temp=temp.next;
+        }
+        return isPalin(s) ? true : false;
+    }
+}
