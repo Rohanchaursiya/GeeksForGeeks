b4cb67729997a0319fed3f284a752b2c67d3e07f
Rohan Kumar Chaursiya <97392604+Rohanchaursiya@users.noreply.github.com>
2024-07-31 23:45:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest Common Prefix of Strings/longest-common-prefix-of-strings.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String arr[] = read.readLine().trim().split(" ");
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonPrefix(arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String longestCommonPrefix(String arr[]) {
+        // code here
+        StringBuilder sb=new StringBuilder();
+        Arrays.sort(arr);
+        String first=arr[0];
+        String last=arr[arr.length-1];
+        for(int i=0;i<Math.min(first.length(),last.length());i++){
+            if(first.charAt(i)!=last.charAt(i)){
+                break;
+            }
+            sb.append(first.charAt(i));
+        }
+        return sb.length()==0 ? "-1" :sb.toString();
+    }
+}
